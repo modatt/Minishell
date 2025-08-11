b789f7c0fef59e966ddd376907183aba52ae2ab6
Hala Meltaha <hmeltaha@c1r7s2.42amman.com>
2025-08-11 20:58:20 +0300

    hd

--- parse/shlvl.c
+++ parse/shlvl.c
@@ -6,7 +6,7 @@
 /*   By: hmeltaha <hmeltaha@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 18:23:59 by hmeltaha          #+#    #+#             */
-/*   Updated: 2025/08/11 17:56:27 by hmeltaha         ###   ########.fr       */
+/*   Updated: 2025/08/11 20:56:59 by hmeltaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,42 @@ void	update_envp_array(t_shell *shell, const char *name, const char *value)
 	}
 	free(prefix);
 }
+ static int 	update_or_add_var_shlvl(char *name, char *value, t_shell *shell)
+{
+	t_env_var	*exist;
+	t_env_var	*new_var;
+
+	exist = find_var(name, shell->env_list);
+	if (exist)
+	{
+		if (value)
+			update_var_value(exist, value);
+		exist->exported = true;
+	}
+	else
+	{
+		new_var = create_var(name, value, true);
+		if (new_var)
+			add_var_to_list(&shell->env_list, new_var);
+		else
+			return (1);
+	}
+	return (0);
+}
+
+ static int 	handle_export_var_shlvl(char *name, char *value, t_shell *shell, int status)
+{
+	if (name == NULL || name[0] == '\0' || !is_name_valid(name))
+	{
+		name_invalid(name, value);
+		status = 1;
+	}
+	else
+	{
+		status = update_or_add_var_shlvl(name, value, shell);
+	}
+	return (status);
+}
 
 //func ---1
 void	update_shlvl(t_shell *shell)
@@ -59,7 +95,7 @@ void	update_shlvl(t_shell *shell)
 	new_val = ft_itoa(shlvl);
 	if (!new_val)
 		return ;
-	handle_export_var_cd("SHLVL", new_val, shell, 0);
+	handle_export_var_shlvl("SHLVL", new_val, shell, 0);
 	update_envp_array(shell, "SHLVL", new_val);
 	free(new_val);
 	free(shlvl_str);
